ad87a0d4cc07b694a82cc43c45b582ecb54acd1b
dj940212 <2902273280@qq.com>
2017-09-03 15:52:46 +0800

    完成首页文件引入

--- mxonline/settings.py
+++ mxonline/settings.py
@@ -128,8 +128,8 @@ USE_L10N = True
 
 USE_TZ = False
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
